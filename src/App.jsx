89aa675822560f89de9df94932509dd72f084aca
Brandon Johnson <johnsbr99@gmail.com>
2025-03-07 07:30:45 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+    return(
+        <>
+        <h1>Hello Worlds!</h1>
+        </>
+    )
+}
+
+/**
+ * Challenge: Start a brand new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
