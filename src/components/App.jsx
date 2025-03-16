b01a3e2c6d504337130243526d9cd1239d500145
Brandon Johnson <johnsbr99@gmail.com>
2025-03-16 07:46:00 -0600

    Refactored to allow holding individual dice on re-roll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,8 @@ import Die from './Die'
 
 export default function App() {
 
-    const [diceArray, setDiceArray] = useState(generatAllNewDice())
+    const [dice, setDice] = useState(generatAllNewDice())
+    
 
     function generatAllNewDice() {
         return new Array(10).fill(0).map(() => ({
@@ -15,23 +16,50 @@ export default function App() {
         }))
     }
 
-    const dice = diceArray.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />)
+    const diceElements = dice.map(dieObj => (
+        <Die 
+            id={dieObj.id}
+            key={dieObj.id} 
+            value={dieObj.value} 
+            isHeld={dieObj.isHeld}
+            handleClick={hold}
+           />)
+    )
+
+    /*
+        -call setDice to trigger re-render of our state variable
+        - map over the array of die objects
+        - if die being iterated over containes a isHeld: false then generate a new value
+    */
 
     function rollDice() {
-        setDiceArray(generatAllNewDice())
+        // setDice(generatAllNewDice())
+        setDice(oldDice => oldDice.map(die =>
+            die.isHeld !== true ?
+                {...die, value: Math.ceil(Math.random() * 6)}:
+                die
+        ))
     }
 
+    function hold(id) {
+        setDice(oldDice => oldDice.map(die => 
+            die.id === id ?
+                {...die, isHeld: !die.isHeld} :
+                die
+        ))
+    }
 
     return(
         <main>
-            <section className="mainContent">
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze 
+            it at its current value between rolls.</p>
+
                 <div className="diceContainer">
-                    {dice}
+                    {diceElements}
                 </div>
 
                 <button id="rollButton" onClick={rollDice}>Roll</button>
-
-            </section>
         </main>
     )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,15 +1,15 @@
+
 export default function Die(props) {
+    
+    const styles = {
+        backgroundColor: props.isHeld ? '#59e391' : 'white'
+    }
+    
 
     return(
-            <button>{props.value}</button>
+            <button 
+                style={styles}
+                onClick={() => props.handleClick(props.id)}
+            >{props.value}</button>
     )
 }
-
-//onClick={handleClick}
-
-//need count function that allows a value of 1-6
-//value should randomly change value onClick
-//die component should be styled with css grid rendering 10 
-//         instances of the die
-//each die should have individual state b/c clicking one button shouldnt 
-//         update state on remaining dice
